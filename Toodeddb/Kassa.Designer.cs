bd8523e36fc89f7d705984a2c62b0d2a615dacfe
Mihhail Lastovski <lastovskim@gmail.com>
2022-11-15 05:51:59 +0200

    upd

--- Toodeddb/Kassa.Designer.cs
+++ Toodeddb/Kassa.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.toodelbl = new System.Windows.Forms.Label();
             this.koguslbl = new System.Windows.Forms.Label();
             this.hindlbl = new System.Windows.Forms.Label();
@@ -37,30 +36,18 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox1.Location = new System.Drawing.Point(352, 43);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(436, 189);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
             // 
             // toodelbl
             // 
             this.toodelbl.AutoSize = true;
             this.toodelbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.toodelbl.Location = new System.Drawing.Point(19, 43);
+            this.toodelbl.Location = new System.Drawing.Point(4, 360);
             this.toodelbl.Name = "toodelbl";
             this.toodelbl.Size = new System.Drawing.Size(107, 18);
             this.toodelbl.TabIndex = 17;
@@ -70,7 +57,7 @@
             // 
             this.koguslbl.AutoSize = true;
             this.koguslbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.koguslbl.Location = new System.Drawing.Point(75, 101);
+            this.koguslbl.Location = new System.Drawing.Point(60, 418);
             this.koguslbl.Name = "koguslbl";
             this.koguslbl.Size = new System.Drawing.Size(51, 18);
             this.koguslbl.TabIndex = 18;
@@ -80,7 +67,7 @@
             // 
             this.hindlbl.AutoSize = true;
             this.hindlbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.hindlbl.Location = new System.Drawing.Point(88, 155);
+            this.hindlbl.Location = new System.Drawing.Point(326, 362);
             this.hindlbl.Name = "hindlbl";
             this.hindlbl.Size = new System.Drawing.Size(38, 18);
             this.hindlbl.TabIndex = 19;
@@ -90,7 +77,7 @@
             // 
             this.kategoorialbl.AutoSize = true;
             this.kategoorialbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.kategoorialbl.Location = new System.Drawing.Point(40, 212);
+            this.kategoorialbl.Location = new System.Drawing.Point(278, 418);
             this.kategoorialbl.Name = "kategoorialbl";
             this.kategoorialbl.Size = new System.Drawing.Size(86, 20);
             this.kategoorialbl.TabIndex = 20;
@@ -100,50 +87,41 @@
             // 
             this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(159, 43);
+            this.label1.Location = new System.Drawing.Point(117, 361);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(148, 19);
+            this.label1.Size = new System.Drawing.Size(125, 19);
             this.label1.TabIndex = 21;
             // 
             // label2
             // 
             this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(159, 100);
+            this.label2.Location = new System.Drawing.Point(117, 418);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(148, 19);
+            this.label2.Size = new System.Drawing.Size(125, 19);
             this.label2.TabIndex = 22;
             // 
             // label3
             // 
             this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(159, 154);
+            this.label3.Location = new System.Drawing.Point(420, 361);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(148, 19);
+            this.label3.Size = new System.Drawing.Size(125, 19);
             this.label3.TabIndex = 23;
             // 
             // label4
             // 
             this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(159, 212);
+            this.label4.Location = new System.Drawing.Point(420, 419);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(148, 19);
+            this.label4.Size = new System.Drawing.Size(125, 19);
             this.label4.TabIndex = 24;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 288);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(776, 150);
-            this.dataGridView1.TabIndex = 25;
-            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(352, 251);
+            this.numericUpDown1.Location = new System.Drawing.Point(813, 358);
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(93, 20);
             this.numericUpDown1.TabIndex = 26;
@@ -151,9 +129,9 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(451, 248);
+            this.button1.Location = new System.Drawing.Point(588, 411);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(175, 28);
+            this.button1.Size = new System.Drawing.Size(156, 28);
             this.button1.TabIndex = 27;
             this.button1.Text = "Lisage toode tšekkis";
             this.button1.UseVisualStyleBackColor = true;
@@ -162,9 +140,9 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.Location = new System.Drawing.Point(632, 248);
+            this.button2.Location = new System.Drawing.Point(769, 411);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(156, 28);
+            this.button2.Size = new System.Drawing.Size(137, 28);
             this.button2.TabIndex = 28;
             this.button2.Text = "Hankige tšekk";
             this.button2.UseVisualStyleBackColor = true;
@@ -174,7 +152,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label5.Location = new System.Drawing.Point(206, 248);
+            this.label5.Location = new System.Drawing.Point(643, 359);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(101, 20);
             this.label5.TabIndex = 29;
@@ -184,12 +162,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(914, 447);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -198,11 +175,8 @@
             this.Controls.Add(this.hindlbl);
             this.Controls.Add(this.koguslbl);
             this.Controls.Add(this.toodelbl);
-            this.Controls.Add(this.pictureBox1);
             this.Name = "Kassa";
             this.Text = "Kassa";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -210,8 +184,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label toodelbl;
         private System.Windows.Forms.Label koguslbl;
         private System.Windows.Forms.Label hindlbl;
@@ -220,7 +192,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
--- Toodeddb/Kassa.cs
+++ Toodeddb/Kassa.cs
@@ -27,27 +27,119 @@ namespace Toodeddb
     public partial class Kassa : Form
     {
         SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\lasto\source\repos\Toodeddb\Toodeddb\AppData\Tooded_DB.mdf;Integrated Security=True");
-        SqlDataAdapter adapter_toode;
-        SqlCommand cmd;
+        SqlDataAdapter adapter_toode, adapter_kat;
+        SqlCommand cmd, cmd2, cmd3;
+        SqlDataReader reader;
+        TabControl kategooriad;
+        DataGridView dataGrid;
         public Kassa()
         {
+            Kategooriad();
             InitializeComponent();
             Naita_Andmed();
         }
+        int kat_Id;
+        public void Kategooriad()
+        {
+            kategooriad = new TabControl();
+            kategooriad.Name = "Kategooriad";
+            kategooriad.Width = 891;
+            kategooriad.Height = 313;
+            connect.Open();
+            adapter_kat = new SqlDataAdapter("SELECT Id, Kategooria_nimetus FROM Kategooriatable", connect);
+            DataTable dt_kat = new DataTable();
+            adapter_kat.Fill(dt_kat);
+            ImageList iconsList = new ImageList();//
+            iconsList.ColorDepth = ColorDepth.Depth32Bit;//
+            iconsList.ImageSize = new Size(25, 25);//
+
+            int i = 0;
+            connect.Close();
+            foreach (DataRow nimetus in dt_kat.Rows)
+            {
+                kategooriad.TabPages.Add((string)nimetus["Kategooria_nimetus"]);
+                iconsList.Images.Add(Image.FromFile(@"..\..\katimg\" + (string)nimetus["Kategooria_nimetus"] + ".jpg"));
+                kategooriad.TabPages[i].ImageIndex = i;//
+                i++;
+                kat_Id = (int)nimetus["Id"];
+                dataGrid = new DataGridView
+                {
+                    Width = 852,
+                    Height = 285,
+                    RowHeadersVisible = false,
+                    ColumnHeadersVisible = false
+                };
+                kategooriad.TabPages[i - 1].Controls.Add(dataGrid);
+                dataGrid.CellClick += DataGrid_CellClick;
+                connect.Open();
+                cmd = new SqlCommand("SELECT id, Pilt FROM Toodetable WHERE Kategooria_id = "+kat_Id, connect);
+                reader = cmd.ExecuteReader();
+                List<string> name_pilt = new List<string>();
+                List<int> toode_id = new List<int>();
+                while (reader.Read())
+                {
+                    name_pilt.Add(reader["Pilt"].ToString());
+                    toode_id.Add((int)reader["id"]);
+                }
+                for (int j = 0; j < name_pilt.Count; j++)
+                {
+                    Image img = Image.FromFile(@"..\..\pictures\" + name_pilt[j]);
+                    DataGridViewImageColumn iconColumn = new DataGridViewImageColumn();
+                    iconColumn.Width = 100;
+                    iconColumn.Image = img;
+                    iconColumn.Name = "Icon_name" + j;
+                    iconColumn.ImageLayout = DataGridViewImageCellLayout.Stretch;
+                    dataGrid.Columns.Add(iconColumn);
+                    dataGrid.Rows[dataGrid.NewRowIndex].Cells["Icon_name"+j].Value = img;
+                    dataGrid.Rows[dataGrid.NewRowIndex].Cells["Icon_name" + j].Tag = toode_id[j];
+                    dataGrid.Rows[dataGrid.NewRowIndex].Cells["Icon_name" + j].ToolTipText = (string)nimetus["Kategooria_nimetus"];
+                    dataGrid.Rows[dataGrid.NewRowIndex].Height = 80;
+                }
+                connect.Close();
+            }
+            kategooriad.ImageList = iconsList;
+            this.Controls.Add(kategooriad);
+        }
+
+        private void DataGrid_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridView data_grid = (DataGridView)sender;
+            try
+            {
+                connect.Open();
+                cmd3 = new SqlCommand("SELECT Toodenimetus, Kogus, Hind FROM Toodetable WHERE id = " + data_grid.CurrentCell.Tag, connect);
+                reader = cmd3.ExecuteReader();
+                while (reader.Read())
+                {
+                    label1.Text = reader["Toodenimetus"].ToString();
+                    label2.Text = reader["Kogus"].ToString();
+                    label3.Text = reader["Hind"].ToString();
+                }
+                label4.Text = data_grid.CurrentCell.ToolTipText;
+                connect.Close();
+                numericUpDown1.Value = 0;
+                cmd2 = new SqlCommand("SELECT [Kogus] FROM [Toodetable] WHERE Toodenimetus = '" + label1.Text + "'", connect);
+                connect.Open();
+                numericUpDown1.Maximum = (int)cmd2.ExecuteScalar();
+                connect.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Vali toode!");
+            }
+        }
+
         public void Naita_Andmed()
         {
             connect.Open();
             DataTable dt_toode = new DataTable();
             adapter_toode = new SqlDataAdapter("SELECT * FROM Toodetable", connect);
             adapter_toode.Fill(dt_toode);
-            dataGridView1.DataSource = dt_toode;
             Image img = Image.FromFile("../../../question.png");
-            Bitmap finalimg = new Bitmap(img, pictureBox1.Width, pictureBox1.Height);
-            pictureBox1.Image = finalimg;
             connect.Close();
         }
 
-        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        /*private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
         {
 
             try
@@ -71,9 +163,11 @@ namespace Toodeddb
                 pictureBox1.Show();
                 int v = Int32.Parse(dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString());
                 cmd = new SqlCommand("SELECT [Kategooria_nimetus] FROM [Kategooriatable] WHERE [Id]=@id", connect);
+                cmd2 = new SqlCommand("SELECT [Kogus] FROM [Toodetable] WHERE Toodenimetus = '" + label1.Text + "'", connect);
                 connect.Open();
                 cmd.Parameters.Add("@id", SqlDbType.Int);
                 cmd.Parameters["@id"].Value = v;
+                numericUpDown1.Maximum = (int)cmd2.ExecuteScalar();
                 label4.Text = cmd.ExecuteScalar().ToString();
                 connect.Close();
             }
@@ -83,11 +177,16 @@ namespace Toodeddb
             }
 
         }
+        */
         Document tsekk = new Document();
         List <string> tsekk_array = new List<string>();
         List<decimal> kokku_hind = new List<decimal>();
         public void button1_Click(object sender, EventArgs e)
         {
+            cmd = new SqlCommand("UPDATE [Toodetable] SET Kogus = Kogus - "+ numericUpDown1.Value + "WHERE Toodenimetus = '" + label1.Text+"'", connect);
+            connect.Open();
+            cmd.ExecuteNonQuery();
+            connect.Close();
             if (numericUpDown1.Value != 0 && label2.Text != "" && label1.Text != "" && label3.Text != "" && label4.Text != "")
             {
                 tsekk_array.Add("Toote nimi " + label1.Text + "; Kogus " + numericUpDown1.Value.ToString() + 
@@ -95,6 +194,7 @@ namespace Toodeddb
                 kokku_hind.Add(Decimal.Parse(label3.Text) * numericUpDown1.Value);
             }
         }
+
 
         private void button2_Click(object sender, EventArgs e)
         {
@@ -131,5 +231,6 @@ namespace Toodeddb
 
             tsekk.Save($@"C:\Users\lasto\source\repos\Toodeddb\Toodeddb\Arved\Tšekk{now.ToString("_ddMMyyyy_HHmmss")}.pdf");
         }
+
     }
 }
